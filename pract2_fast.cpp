0fefcf66ddf9c4750d6276deceeb03641bc13abf
Akash_Badadani <akashsgbadadani@gmail.com>
2022-12-29 20:50:03 +0530

    practice 2.1, with fastest method

---
+++ pract2_fast.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n,m;
+     cout<<"enter the number of test cases"<<endl;
+    cin>>m;
+    
+    for(int k=0;k<m; k++)
+    {
+    cout<<"enter the number of elements in array(odd)"<<endl;
+    cin>>n;
+    cout<<"Enter the elements in an array"<<endl;
+    int a[n];
+    int ans = 0;
+    
+    for(int i = 0; i<n; i++)
+    {
+        cin>>a[i];
+        ans = ans^a[i];
+    }
+
+        cout<<"unique element found is "<<ans<<endl;
+}
+    return 0;
+}
