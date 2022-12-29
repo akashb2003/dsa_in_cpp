661b0368c25237d6a3c7e435d10e14d9975e923a
Akash_Badadani <akashsgbadadani@gmail.com>
2022-12-30 00:49:36 +0530

    practice 2.1, with fastest method

--- fast_pract2.cpp
+++ fast_pract2.cpp
@@ -7,7 +7,7 @@ int main()
     int n,m;
      cout<<"enter the number of test cases"<<endl;
     cin>>m;
-    
+
     for(int k=0;k<m; k++)
     {
     cout<<"enter the number of elements in array(odd)"<<endl;
@@ -15,7 +15,7 @@ int main()
     cout<<"Enter the elements in an array"<<endl;
     int a[n];
     int ans = 0;
-    
+
     for(int i = 0; i<n; i++)
     {
         cin>>a[i];
@@ -26,3 +26,4 @@ int main()
 }
     return 0;
 }
+
